86e75ac8e3a7cb1a20c8fad450679cd73fe8e2f7
MikaelMenez <mixaelmenezes@gmail.com>
2025-09-17 00:09:30 -0300

    início

---
+++ bancodedados.py
@@ -0,0 +1,42 @@
+import sqlite3
+
+sql_connection=sqlite3.connect('app.db',check_same_thread=False)
+sql_command_handler=sql_connection.cursor()
+def create_table():
+    
+    sql_command_handler.execute('''
+            CREATE TABLE IF NOT EXISTS pontos (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            titulo TEXT NOT NULL,
+            texto TEXT NOT NULL,
+            imagem_path TEXT NOT NULL,
+            data_criacao DATETIME DEFAULT CURRENT_TIMESTAMP,
+            cord_x REAL NOT NULL,
+            cord_y REAL NOT NULL
+        )
+    ''')
+
+    sql_connection.commit()
+def acessar_item(id:int)->list:
+    sql_command_handler.execute(f"SELECT * FROM pontos WHERE id=?",(id))
+    sql_connection.commit()
+
+    return sql_command_handler.fetchall()
+def adicionar_item(titulo:str,texto:str,imagem_path:str,data_criacao:str,cord_x:float,cord_y:float):
+    sql_command_handler.execute(f"INSERT INTO pontos (titulo,texto,imagem_path,data_criacao,cord_x,cord_y) VALUES (?,?,?,?,?,?)",(titulo,texto,imagem_path,data_criacao,cord_x,cord_y))
+    sql_connection.commit()
+
+def deletar_item(id:int):
+    sql_command_handler.execute(f'DELETE FROM pontos WHERE id=?',(id))
+    sql_connection.commit()
+def deletar_tudo():
+    sql_command_handler.execute(f'DELETE FROM pontos')
+    sql_connection.commit()
+
+def atualizar_item(id:int,titulo:str,texto:str,imagem_path:str,data_criacao:str,cord_x:float,cord_y:float):
+    sql_command_handler.execute(f'UPDATE pontos SET titulo=?,texto=?,imagem_path=?,data_criacao=?,cord_x=?,cord_y=? WHERE id=?',(titulo,texto,imagem_path,data_criacao,cord_x,cord_y,id))
+    sql_connection.commit()
+
+def acessar_todos()->list:
+    sql_command_handler.execute("SELECT * FROM pontos")
+    return sql_command_handler.fetchall()
---
+++ main.py
@@ -0,0 +1,64 @@
+from fastapi import FastAPI,Request,Form,File,UploadFile,HTTPException
+from fastapi.responses import HTMLResponse,RedirectResponse
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+import folium
+from bancodedados import *
+from pathlib import Path
+import datetime
+import shutil
+
+mundo_map = folium.Map(zoom_start=3)
+create_table()
+templates=Jinja2Templates(directory="templates")
+mundo_map.save("templates/mapa_mundi.html")
+app=FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+@app.get("/",response_class=HTMLResponse)
+def renderizar_mapa(request:Request):
+    pontos = acessar_todos()
+    for ponto in pontos:
+        folium.Marker(
+        location=[ponto[5], ponto[6]],
+        popup=f"<img src={ponto[3]} ><b>{ponto[1]}</b><br>{ponto[2]}<br><b>criado em {ponto[4]}</b>",
+        tooltip="Clique para mais info",
+        icon=folium.Icon(color="green")
+        ).add_to(mundo_map)
+    mundo_map.save("templates/mapa_mundi.html")
+    context={
+        "request": request,  # OBRIGATÓRIO para Jinja2
+        "nome": "Visitante",
+        "data_hora": "16/09/2025",
+        "usuario_logado": False
+    }
+    return templates.TemplateResponse("mapa_mundi.html",context)
+@app.get("/adicionar_pontos",response_class=HTMLResponse)
+def abrir_formulario(request:Request):
+    return templates.TemplateResponse("adicionar_pontos.html",{"request":request})
+@app.post("/submeter_pontos",response_class=HTMLResponse)
+def adicionar_pontos(request:Request,titulo:str=Form(...),texto:str=Form(...),cord_x:float=Form(...),cord_y:float=Form(...),imagem: UploadFile = File(...)):
+    context={
+        "request": request,  # OBRIGATÓRIO para Jinja2
+        "nome": "Visitante",
+        "data_hora": "16/09/2025",
+        "usuario_logado": False
+    }
+    if not imagem.content_type.startswith("image/"):
+        raise HTTPException(400, "Arquivo deve ser uma imagem")
+    data_criacao=datetime.date.today()
+    image=imagem.filename
+    nome_arquivo=f'{data_criacao}_{image}'
+    caminho_arquivo = f'static/images/{nome_arquivo}'
+    Path("static/images").mkdir(parents=True, exist_ok=True)
+
+    # Salvar o arquivo
+    with open(caminho_arquivo, "wb") as buffer:
+        shutil.copyfileobj(imagem.file, buffer)
+    adicionar_item(titulo=titulo,texto=texto,imagem_path=caminho_arquivo,data_criacao=data_criacao,cord_x=cord_x,cord_y=cord_y)
+    
+    return RedirectResponse(url="/sucesso",status_code=303)
+@app.get("/sucesso",response_class=HTMLResponse)
+def sucesso(request:Request,):
+    return templates.TemplateResponse("sucesso.html",{"request":request})
+
+
