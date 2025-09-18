87ab97ac278dd08f895efe4feeda4088dee42d09
MikaelMenez <mixaelmenezes@gmail.com>
2025-09-18 00:28:13 -0300

    cores no db e remover adicionado

--- bancodedados.py
+++ bancodedados.py
@@ -12,7 +12,8 @@ def create_table():
             imagem_path TEXT NOT NULL,
             data_criacao DATETIME DEFAULT CURRENT_TIMESTAMP,
             cord_x REAL NOT NULL,
-            cord_y REAL NOT NULL
+            cord_y REAL NOT NULL,
+            cor TEXT NOT NULL
         )
     ''')
 
@@ -22,19 +23,19 @@ def acessar_item(id:int)->list:
     sql_connection.commit()
 
     return sql_command_handler.fetchall()
-def adicionar_item(titulo:str,texto:str,imagem_path:str,data_criacao:str,cord_x:float,cord_y:float):
-    sql_command_handler.execute(f"INSERT INTO pontos (titulo,texto,imagem_path,data_criacao,cord_x,cord_y) VALUES (?,?,?,?,?,?)",(titulo,texto,imagem_path,data_criacao,cord_x,cord_y))
+def adicionar_item(titulo:str,texto:str,imagem_path:str,data_criacao:str,cord_x:float,cord_y:float,cor:str):
+    sql_command_handler.execute(f"INSERT INTO pontos (titulo,texto,imagem_path,data_criacao,cord_x,cord_y,cor) VALUES (?,?,?,?,?,?,?)",(titulo,texto,imagem_path,data_criacao,cord_x,cord_y,cor))
     sql_connection.commit()
 
 def deletar_item(id:int):
-    sql_command_handler.execute(f'DELETE FROM pontos WHERE id=?',(id))
+    sql_command_handler.execute(f'DELETE FROM pontos WHERE id=?',(id,))
     sql_connection.commit()
 def deletar_tudo():
     sql_command_handler.execute(f'DELETE FROM pontos')
     sql_connection.commit()
 
-def atualizar_item(id:int,titulo:str,texto:str,imagem_path:str,data_criacao:str,cord_x:float,cord_y:float):
-    sql_command_handler.execute(f'UPDATE pontos SET titulo=?,texto=?,imagem_path=?,data_criacao=?,cord_x=?,cord_y=? WHERE id=?',(titulo,texto,imagem_path,data_criacao,cord_x,cord_y,id))
+def atualizar_item(id:int,titulo:str,texto:str,imagem_path:str,data_criacao:str,cord_x:float,cord_y:float,cor:str):
+    sql_command_handler.execute(f'UPDATE pontos SET titulo=?,texto=?,imagem_path=?,data_criacao=?,cord_x=?,cord_y=?,cor=? WHERE id=?',(titulo,texto,imagem_path,data_criacao,cord_x,cord_y,cor,id))
     sql_connection.commit()
 
 def acessar_todos()->list:
@@ -50,7 +51,8 @@ def transformar_em_json(lista_tuplas):
             "imagem_path": item[3],
             "data_criacao": item[4],
             "cord_x": item[5],
-            "cord_y": item[6]
+            "cord_y": item[6],
+            "cor":item[7]
         }
         lista_json.append(d)
     return lista_json
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from fastapi import FastAPI,Request,Form,File,UploadFile,HTTPException
-from fastapi.responses import HTMLResponse,RedirectResponse
+from fastapi import FastAPI,Request,Form,File,UploadFile,HTTPException,status
+from fastapi.responses import HTMLResponse,RedirectResponse,JSONResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 import folium
@@ -9,7 +9,7 @@ import datetime
 import shutil
 import json
 
-mundo_map = folium.Map(zoom_start=3)
+mundo_map = folium.Map(location=[-20,-20],zoom_start=2.5)
 create_table()
 templates=Jinja2Templates(directory="templates")
 mundo_map.save("templates/mapa_mundi.html")
@@ -21,9 +21,9 @@ def renderizar_mapa(request:Request):
     for ponto in pontos:
         folium.Marker(
         location=[ponto[5], ponto[6]],
-        popup=f"<img src={ponto[3]} ><b>{ponto[1]}</b><br>{ponto[2]}<br><b>criado em {ponto[4]}</b>",
+        popup=f"<img src={ponto[3]} style=\"width:7vw; height:auto\";/><b>{ponto[1]}</b><br>{ponto[2]}<br><b>criado em {ponto[4]}</b>",
         tooltip="Clique para mais info",
-        icon=folium.Icon(color="green")
+        icon=folium.Icon(color=ponto[7])
         ).add_to(mundo_map)
     mundo_map.save("templates/mapa_mundi.html")
     context={
@@ -37,7 +37,7 @@ def renderizar_mapa(request:Request):
 def abrir_formulario(request:Request):
     return templates.TemplateResponse("adicionar_pontos.html",{"request":request})
 @app.post("/submeter_pontos",response_class=HTMLResponse)
-def adicionar_pontos(request:Request,titulo:str=Form(...),texto:str=Form(...),cord_x:float=Form(...),cord_y:float=Form(...),imagem: UploadFile = File(...)):
+def adicionar_pontos(request:Request,titulo:str=Form(...),texto:str=Form(...),cord_x:float=Form(...),cord_y:float=Form(...),imagem: UploadFile = File(...),cor:str=Form(...)):
     context={
         "request": request,  # OBRIGATÓRIO para Jinja2
         "nome": "Visitante",
@@ -55,17 +55,17 @@ def adicionar_pontos(request:Request,titulo:str=Form(...),texto:str=Form(...),co
     # Salvar o arquivo
     with open(caminho_arquivo, "wb") as buffer:
         shutil.copyfileobj(imagem.file, buffer)
-    adicionar_item(titulo=titulo,texto=texto,imagem_path=caminho_arquivo,data_criacao=data_criacao,cord_x=cord_x,cord_y=cord_y)
-    
+    adicionar_item(titulo=titulo,texto=texto,imagem_path=caminho_arquivo,data_criacao=data_criacao,cord_x=cord_x,cord_y=cord_y,cor=cor)
+        
     return RedirectResponse(url="/sucesso",status_code=303)
 @app.get("/remove",response_class=HTMLResponse)
 def mostrar_remocao(request:Request):
-    return templates.TemplateResponse("remover_pontos.html",{"request":request})
-@app.get("/elementos")
-def elementos(request:Request):
-    lista=acessar_todos()
-    lista=transformar_em_json(lista)
-    return lista
+    items=transformar_em_json(acessar_todos())
+    return templates.TemplateResponse("remover_pontos.html",{"request":request,"items":items})
+@app.delete("/deletar")
+def deletar(request:Request,id_item:int):
+    deletar_item(id_item)
+    return JSONResponse(content={"message":"removido com sucesso"},status_code=status.HTTP_200_OK)
 @app.get("/sucesso",response_class=HTMLResponse)
 def sucesso(request:Request,):
     return templates.TemplateResponse("sucesso.html",{"request":request})
